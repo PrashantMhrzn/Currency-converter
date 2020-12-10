32c37234b2e1cc14fa16a8e0d7995a3f277efd00
Prashant Maharjan <prashantmaharjan5@gmail.com>
2020-12-10 18:41:52 +0545

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 try:
     while True:
         symbol = input("Enter the symbol of currency:(eg:EUR,JPY,NPR) ").upper()
-        url = f"https://api.currencyfreaks.com/latest?apikey=9fc35b0d31f24b21b373045218753cd1&symbols={symbol}"
+        url = f"https://api.currencyfreaks.com/latest?apikey={YOUR_API_KEY}&symbols={symbol}"
         res = requests.get(url)
         text = res.json()
         base = text['base']
